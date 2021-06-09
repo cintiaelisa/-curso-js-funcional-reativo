bd16b87d455ddaba8a528a7dc401f4fc5bf34ddd
cintiaelisa@gmail.com <cintiaelisa@gmail.com>
2021-06-08 23:31:41 -0300

    Projeto 1 - Agrupando palavras #1

--- projeto_1/index.js
+++ projeto_1/index.js
@@ -9,6 +9,18 @@ const simbolos = [
     '(', ')'
 ]
 
+function agruparPalavras(palavras) {
+    return palavras.reduce((agrupamento, palavra) => {
+        const p = palavra.toLowerCase()
+        if(agrupamento[p]) {
+            agrupamento[p] += 1
+        } else {
+            agrupamento[p] = 1
+        }
+        return agrupamento
+    }, {} )
+}
+
 fn.lerDiretorio(caminho)
     .then(fn.elementosTerminadosEm('.srt'))
     .then(fn.lerArquivos)
@@ -21,4 +33,6 @@ fn.lerDiretorio(caminho)
     .then(fn.mesclarElementos)
     .then(fn.separarTextoPor(' '))
     .then(fn.removerElementosSeVazio)
+    .then(fn.removerElementosApenasNumeros)
+    .then(agruparPalavras)
     .then(console.log)
